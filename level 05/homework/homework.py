274727c25ac43804905f70e1d471c752065dc683
LukePGx <lukamindiashvili33@gmail.com>
2024-06-01 02:31:28 +0400

    fourth commit

---
+++ level 05/homework/homework.py
@@ -0,0 +1,18 @@
+# in here, we have only used used the snakecase from best practices. as long as we dont count this comment
+# we used snakecase so we could actually see the words. without the snakecase it would be dogname, or DogName, but that is camelcase which is only used in java.
+# and we used the comment so we could read what what happened in that part, as computer ignores the comments, its only for us humans to read what we did on there.
+
+
+
+name = "luka"
+dog_name = "lusi"
+grade = "8th"
+academy = "GOA"
+study_to_be = "game developer"
+
+
+sentence = f"my name is {name}, i have a dog, her name is {dog_name}. im in {grade} grade and i study at {academy}, studying to become a {study_to_be}"
+
+
+print(sentence)
+
