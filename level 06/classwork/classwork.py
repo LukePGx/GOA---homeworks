a697fddc5843f63ded46ec19bd1f9d41aa2826e7
LukePGx <lukamindiashvili33@gmail.com>
2024-06-06 01:08:47 +0400

    sixth commit

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,9 @@
+#the names of the dogs.
+
+
+#dog_name1 = "Rick"
+#dog_name2 = "John"
+#dog_name2 = "Max"
+
+#print(f"first dog's name is {dog_name1}, second dog's name is {dog_name2}.")
+
---
+++ level 06/homework/homework.py
@@ -0,0 +1,37 @@
+num1 = 5
+num2 = 10
+num3 = 20
+
+print(num1 + num2 + num3) #string
+print(num1 * num2 * num3) #string
+print(num1 - num2 - num3) #string
+print(num1 / num2 / num3) #float
+
+
+#making an account
+
+#name
+
+var1 = input("enter your name: ")
+
+print(f"name: {var1}")
+
+#last name
+
+var2 = input("enter your lastname: ")
+
+print(f"last name: {var2}")
+
+#age
+
+num1 = int(input("enter your age: "))
+
+print(f"age: {num1}")
+
+#password
+
+num2 = int(input("enter new password: "))
+
+print(f"password: {num2}")
+
+print("account created succesfully!")
