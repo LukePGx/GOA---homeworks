50a5a48c8d7cc0c99cc2786cf07577b8352ed0d4
LukePGx <lukamindiashvili33@gmail.com>
2024-06-12 20:21:18 +0400

    commit

---
+++ level 08/homework/homework.py
@@ -0,0 +1,24 @@
+# fruit = "apple"
+# balance = 1000
+# height = 2.34
+# is_programmer = True
+
+# print(type(fruit))
+# print(type(balance))
+# print(type(height))
+# print(type(is_programmer))
+
+
+
+print(10 < 20) #True
+print(10 > 20) #False
+print(10 == 20) #False
+
+print(True or False) #True
+print(True and False) #False
+
+print(10 < 20 or 10 < 20) #False
+print(10 < 20 and 10 > 20)
+
+
+
