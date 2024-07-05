7bac95c0dd075acc8560f67059a38f7714611d01
LukePGx <lukamindiashvili33@gmail.com>
2024-07-05 22:48:39 +0400

    commit

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,36 @@
+# 1
+
+fruits = ["banana", "apple", "orange", "lime", "grape"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
+
+# 2
+
+fruits = ["banana", "apple", "orange", "lime", "grape"]
+
+fruits[1] = "lemon"
+fruits[4] = "mango"
+
+print(fruits)
+
+# 3
+
+fruits = ["banana", "apple", "orange", "lime", "grape"]
+
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+print(fruits[-4])
+print(fruits[-5])
+
+
+fruits = ["banana", "apple", "orange", "lime", "grape"]
+
+fruits[-4] = "lemon"
+fruits[-1] = "mango"
+
+print(fruits)
